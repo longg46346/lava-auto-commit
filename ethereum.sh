da273548f68c72cabae1c62acc64e026c594a3a1
longg46346 <147234020+longg46346@users.noreply.github.com>
2024-03-25 11:18:04 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-4dde12d9-b411-483c-b3ab-847136473343/"
 
 OUTPUT_DIR="logs"
 
